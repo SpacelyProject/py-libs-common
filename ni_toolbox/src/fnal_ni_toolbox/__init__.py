e66b5df5c7957e623078dffa0539a557cdd97849
kiler129 <grzegorz@noflash.pl>
2023-06-02 12:27:51 -0500

    Rename ni_toolbox folder to nitoolbox (PEP compiance)

--- ni_toolbox/src/fnal_ni_toolbox/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file was intentionally left empty
-# See https://packaging.python.org/en/latest/tutorials/packaging-projects/ for details
---
+++ nitoolbox/src/fnal_ni_toolbox/__init__.py
@@ -0,0 +1,6 @@
+# This file defines PUBLIC interface of your module
+# It also defines any MODULE initialization steps (there usually none)
+
+from .nidcpower import *
+
+# @todo this needs a cleanup as the nidcpower.py has very generic names which will name-conflict with other things
