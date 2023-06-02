ee0f0a6af85da4cfd205fe8504c8419dea98576f
kiler129 <grzegorz@noflash.pl>
2023-06-02 12:29:06 -0500

    [libprologix] PEP compliance

--- libprologix/src/fnal_libprologix/__init__.py
+++ libprologix/src/fnal_libprologix/__init__.py
@@ -1,2 +1,13 @@
-# This file was intentionally left empty
-# See https://packaging.python.org/en/latest/tutorials/packaging-projects/ for details
+# This file defines PUBLIC interface of your module
+# It also defines any MODULE initialization steps (there usually none)
+
+from .prologixdevice import (
+    PrologixDevice
+)
+
+# List all symbols that should be externally-importable symbols here
+# You shouldn't export things that are internal to only this module and only used by the library and not someone using
+#  this library.
+__all__ = [
+    "PrologixDevice"
+]
