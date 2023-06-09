03cdf19a59045c1c4208e2df6c781c734a712f02
greg129 (FNAL) <gzdano2@uic.edu>
2023-06-09 13:31:37 -0500

    [LogWizard] Export log levels

--- liblogwizard/src/fnal_log_wizard/__init__.py
+++ liblogwizard/src/fnal_log_wizard/__init__.py
@@ -3,6 +3,7 @@
 
 # todo 1 class per file will probably be clener
 from .logger import *
+from .levels import *
 
 # List all symbols that should be externally-importable symbols here
 # You shouldn't export things that are internal to only this module and only used by the library and not someone using
@@ -14,4 +15,14 @@ __all__ = [
     'ChainLogger',
     'PlainLogger',
     'AnsiTerminalLogger',
+
+    # Constants for log levels
+    'LOG_EMERG',
+    'LOG_ALERT',
+    'LOG_CRIT',
+    'LOG_ERR',
+    'LOG_WARN',
+    'LOG_NOTICE',
+    'LOG_INFO',
+    'LOG_DEBUG'
 ]
